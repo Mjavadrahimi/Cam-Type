105876cd5505d5cde2c55b0239e1d7bc28a6d99a
Mjavadrahimi <manre80@gmail.com>
2024-04-02 06:56:15 +0330

    - hand trail to 28*28 image added - basic gesture commands added

---
+++ models/character_recognize.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+from PIL import Image
+from matplotlib import pyplot as plt
+from torchvision import transforms
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.l1 = nn.Linear(input_size, hidden_size1)
+        self.l2 = nn.Linear(hidden_size1, hidden_size2)
+        self.l3 = nn.Linear(hidden_size2, 26)
+        self.dropout = nn.Dropout(0.2)
+        self.relu = nn.ReLU()
+        self.sig = nn.Sigmoid()
+
+    def forward(self, input: torch.Tensor, train=False):
+        input = input.view(-1, 1, 28, 28)
+        # size = X * 28*28 * 1
+
+        output = self.relu(self.pool(self.conv1(input)))
+        # size = X * 12*12 * 6
+        output = self.relu(self.pool(self.conv2(output)))
+        # size = X * 4*4 * 16
+
+        output = output.view(-1, 16 * 4 * 4)  # flattening
+        # size = X * 256
+
+        if train: output = self.dropout(output)
+        output = self.l1(output)
+        output = self.relu(output)
+
+        if train: output = self.dropout(output)
+        output = self.l2(output)
+        output = self.relu(output)
+
+        if train: output = self.dropout(output)
+        output = self.l3(output)
+        output = self.sig(output)
+        return output
+
+
+def image_show(image: Image.Image, title=''):
+    plt.imshow(image, cmap='grey')
+    if title:
+        plt.title(title)
+    plt.show()
+
+
+CODE_TO_CHAR = {i: chr(ord('A') + i) for i in range(26)}
+CHAR_TO_CODE = {chr(ord('A') + i): i for i in range(26)}
+
+
+class CharacterRecognize:
+    def __init__(self, PATH):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = Model(256, 120, 80).to(self.device)
+        self.model.load_state_dict(torch.load(PATH))
+        self.model.eval()
+
+    def image_to_tensor(self, image: Image.Image):  # MinMax_scale_image #grey_scale #to_tensor #to_device
+        transform = transforms.ToTensor()
+        image_gery = image.convert("L")
+        torch_image = transform(image_gery).to(self.device)
+        return torch_image
+
+    def predict(self, image: Image.Image, top_k=None):
+        torch_image = self.image_to_tensor(image)
+        pred = self.model(torch_image)
+        if top_k:
+            top_values, top_indices = torch.topk(pred, k=top_k, sorted=True)
+            output = []
+            for value, index in zip(top_values[0], top_indices[0]):
+                output.append((CODE_TO_CHAR[index.item()], value.item()))
+            return output
+        else:
+            return CODE_TO_CHAR[torch.argmax(pred).item()], torch.max(pred).item()
+
+
+if __name__ == '__main__':
+    PATH = 'character_recognizer_model.pth'
+    obj = CharacterRecognize(PATH)
---
+++ models/hand_gesture.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.L1 = nn.Linear(input_size, hidden_size)
+        self.L2 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        output = self.L1(input)
+        output = self.relu(output)
+        output = self.L2(output)
+        output = self.sigmoid(output)
+        return output
+
+
+def landmark_bbox(hand_landmarks, height, width) -> [float, float, float, float]:
+    x_min, y_min = width, height
+    x_max, y_max = 0, 0
+    for landmark in hand_landmarks.landmark:
+        x, y = landmark.x * width, landmark.y * height
+        x_min = min(x_min, x)
+        y_min = min(y_min, y)
+        x_max = max(x_max, x)
+        y_max = max(y_max, y)
+    return x_min, y_min, x_max, y_max
+
+
+def normalized_landmarks(hand_landmarks, height, width) -> list[(float, float)]:  # mp.landmark => [(int, int)]
+    bbox = landmark_bbox(hand_landmarks, height, width)
+    x_min, y_min, x_max, y_max = bbox
+    bbox_W, bbox_H = (x_max - x_min), (y_max - y_min)
+    normalized_landmarks = []
+    for landmark in hand_landmarks.landmark:
+        x = (landmark.x * width - x_min) / bbox_W
+        y = (landmark.y * height - y_min) / bbox_H
+        normalized_landmarks.append((x, y))
+    return normalized_landmarks
+
+
+def to_tensor(positions, device):  # [(float, float)] => tensor[42]
+    positions = torch.Tensor(positions).type(torch.float).flatten().to(device)
+    return positions
+
+
+CODE_TO_STRING = {
+    0: 'clenched_fist',
+    1: 'index_finger_up',
+    2: 'open_palm'
+}
+
+STRING_TO_CODE = {
+    'clenched_fist': 0,
+    'index_finger_up': 1,
+    'open_palm': 2
+}
+
+
+class HandGesture:
+    def __init__(self, PATH):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = Model(42, 20, 3).to(self.device)
+        self.model.load_state_dict(torch.load(PATH))
+        self.model.eval()
+
+    def predict(self, landmark: list[(float, float)]) -> (str, float):
+        input = to_tensor(landmark, self.device)
+        pred = self.model(input)
+        pred_string, pred_confidence = CODE_TO_STRING[pred.argmax().item()], pred.max().item()
+        return pred_string, pred_confidence
+
+
+if __name__ == '__main__':
+    # load model
+    MODEL_PATH = 'hand_gesture_model.pth'
+    obj = HandGesture(MODEL_PATH)
